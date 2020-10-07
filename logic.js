fe0e883c452e049330e02328466bc357a2290eb0
Ebba Pennbrant <ebba.pennbrant@medieinstitutet.se>
2020-10-07 15:33:33 +0200

    övning1
    färdig med uppgift1

---
+++ logic.js
@@ -0,0 +1,26 @@
+function fullName(firstName, lastName) {
+    alert(firstName + ' ' + lastName)
+}
+
+fullName('Ebba', 'Pennbrant');
+
+const logButton = document.getElementById('logButton')
+
+logButton.addEventListener('click', logColor)
+
+let favColor = prompt('Your favorite color:')
+
+function logColor() {
+    console.log(favColor)
+    
+}
+console.log ('Ebba')
+
+let favFood = prompt ('What is your favourite food?')
+let = alert(favFood)
+console.log(favFood)
+
+let numberOne = Number (prompt ('Enter the first digit:'))
+let numberTwo = Number (prompt ('Enter the second digit:'))
+
+let sum = alert(numberOne + numberTwo)
