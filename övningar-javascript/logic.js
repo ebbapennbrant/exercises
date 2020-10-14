238d84053e5110340a40ea5e296a37b7f7e6f9eb
Ebba Pennbrant <ebba.pennbrant@medieinstitutet.se>
2020-10-14 15:47:28 +0200

    vidare uppgifter KLARA

--- övningar-javascript/logic.js
+++ övningar-javascript/logic.js
@@ -1,10 +1,12 @@
-function fullName(firstName, lastName) {
+// Övning 1
+
+/* function fullName(firstName, lastName) {
     alert(firstName + ' ' + lastName)
 }
 
-fullName('Ebba', 'Pennbrant');
+fullName('Ebba', 'Pennbrant'); */
 
-const logButton = document.getElementById('logButton')
+/* const logButton = document.getElementById('logButton')
 
 logButton.addEventListener('click', logColor)
 
@@ -12,15 +14,60 @@ let favColor = prompt('Your favorite color:')
 
 function logColor() {
     console.log(favColor)
-    
-}
-console.log ('Ebba')
+} */
 
-let favFood = prompt ('What is your favourite food?')
+/* console.log ('Ebba') */
+
+/* let favFood = prompt ('What is your favourite food?')
 let = alert(favFood)
 console.log(favFood)
-
-let numberOne = Number (prompt ('Enter the first digit:'))
+ */
+/* let numberOne = Number (prompt ('Enter the first digit:'))
 let numberTwo = Number (prompt ('Enter the second digit:'))
 
 let sum = alert(numberOne + numberTwo)
+ */
+// Vidare övningsuppgifter 1
+
+/* const textButton = document.getElementById('textButton')
+textButton.addEventListener('click', onclick)
+
+function onclick() {
+    document.getElementById('text').innerText = prompt('Enter new text:')
+} */
+
+// Vidare övningsuppgifter 2
+
+const button = document.getElementById('button')
+button.addEventListener('click', colorSwitch)
+
+function colorSwitch() {
+    let divOne = document.getElementById('divOne')
+    let textOne = document.getElementById('textOne')
+
+    if(divOne.style.backgroundColor == 'blue') {
+        divOne.style.backgroundColor = 'yellow'
+        textOne.innerText = 'YELLOW'
+    } else if(divOne.style.backgroundColor == 'yellow') {
+        divOne.style.backgroundColor = 'red'
+        textOne.innerText = 'RED'
+    } else {
+        divOne.style.backgroundColor = 'blue'
+        textOne.innerText = 'BLUE'
+    }
+
+    let divTwo = document.getElementById('divTwo')
+    let textTwo = document.getElementById('textTwo')
+
+    if(divTwo.style.backgroundColor == 'green') {
+        divTwo.style.backgroundColor = 'pink'
+        textTwo.innerText = 'PINK'
+    } else if(divTwo.style.backgroundColor == 'pink') {
+        divTwo.style.backgroundColor = 'purple'
+        textTwo.innerText = 'PURPLE'
+    } else {
+        divTwo.style.backgroundColor = 'green'
+        textTwo.innerText = 'GREEN'
+    }
+    
+}
